8ee9718d89ecb54c840c9f8bc3c78d149bc55c53
WasAlexHere <sethtorston@mail.ru>
2021-12-18 19:38:23 +0300

    [*] modified the view of telegram bot

--- JapDicTelegramBot.py
+++ JapDicTelegramBot.py
@@ -1,34 +1,43 @@
 import telebot
 import requests
 from bs4 import BeautifulSoup
+from profile import KEY
 
-token = '961588413:AAE2FWmA_sSvxYqX1cPbjFXnVWbJQTw5LNQ'
+token = KEY
 bot = telebot.TeleBot(token)
+
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Для начала введите слова чтобы его перевести')
+    bot.send_message(message.chat.id, 'Для начала введите слова '
+                                      'чтобы его перевести')
+
 
 @bot.message_handler(commands=['help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Я твой персональный русско-японский словать! '
-                                      'Попробую рассказать тебе как тут все работает! '
-                                      'Для начала, пришли мне слово, которое хочешь перевести, а остальное сделаю я! '
-                                      'Ты можешь вводить слова на русском, ромадзи, хирагане или катакан!')
+    bot.send_message(message.chat.id,
+                     'Привет! Я твой персональный русско-японский словать!'
+                     'Попробую рассказать тебе как тут все работает! '
+                     'Для начала, пришли мне слово, которое хочешь перевести, '
+                     'а остальное сделаю я! Ты можешь вводить слова на русском, '
+                     'ромадзи, хирагане или катакан!')
 
-@bot.message_handler(content_types=['photo','audio','sticker','video','video_note','voice','location','contact'])
+
+@bot.message_handler(content_types=['photo', 'audio', 'sticker', 'video',
+                                    'video_note', 'voice', 'location', 'contact'])
 def handle_text(message):
-    bot.send_message(message.chat.id, "Увы, так переводить я пока не умею...")
+    bot.send_message(message.chat.id, 'Увы, так переводить я пока не умею...')
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     payload = {'q': message.text, 'pg': '0', 'dic_yarxi': '1', 'sw': '1792'}
-
-    #для отладки в терминале
-    #print("Перевод для слова:",message.text,"\n")
+    # print(f'Перевод для слова: {message.text}\n')
 
     site = 'http://www.jardic.ru/search/search_r.php'
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/80.0.3987.149 Safari/537.36'}
 
     page = requests.get(site, params=payload, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -37,13 +46,10 @@ def handle_text(message):
     try:
         for i in range(10):
             bot.send_message(message.chat.id, result[i].text)
-
-            #для отладки в терминале
-            #print("Перевод:", result[i].text)
+            # print(f'Перевод: {result[i].text}')
     except IndexError:
-        bot.send_message(message.chat.id, "Увы, больше ничего нет...")
+        bot.send_message(message.chat.id, 'Увы, больше ничего нет...')
+        # print('Увы, больше ничего нет...\n')
 
-        #для отладки в терминале
-        #print("Увы, больше ничего нет...","\n")
 
-bot.polling(none_stop=True,interval=0)
+bot.polling(none_stop=True, interval=0)
